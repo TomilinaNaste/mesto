eba245c3e00733d67b03b5bf9febec568ff8fe13
Anastacia Tomilina <anastacia.tomilina@yandex.ru>
2023-03-10 20:14:48 +0300

    feat: внести первые изменения по ревью1

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,30 @@
-const PopupElement = document.querySelector('.popup');
-const PopupElementOpen = document.querySelector('.profile__edit-button');
-const PopupElementClose = PopupElement.querySelector('.popup__close-button');
+const popupElement = document.querySelector('.popup');
+const popupElementOpen = document.querySelector('.profile__edit-button');
+const popupElementClose = popupElement.querySelector('.popup__close-button');
 
 let formElement = document.querySelector('.form');
 let nameInput = formElement.querySelector('.form__item');
-let jobInput = formElement.querySelector('.form__item-job');
+let jobInput = formElement.querySelector('.form__item_job');
 let addNameInputInProfileTitle = document.querySelector('.profile__title');
 let addNameInputInProfileSubTitle = document.querySelector('.profile__subtitle');
 
-
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-
+function handleFormSubmit(evt) {
+  evt.preventDefault();
   addNameInputInProfileTitle.textContent = nameInput.value;
   addNameInputInProfileSubTitle.textContent = jobInput.value;
 
-  ItsPopupClose ();
+  itsPopupClose();
 }
-PopupElement.addEventListener('submit', handleFormSubmit);
-
+formElement.addEventListener('submit', handleFormSubmit);
 // функция открытия поп-апа
-const ItsPopupOpen = function () {
-  PopupElement.classList.add('popup_open');
+const itsPopupOpen = function () {
+  popupElement.classList.add('popup_open');
   nameInput.value = addNameInputInProfileTitle.textContent;
   jobInput.value = addNameInputInProfileSubTitle.textContent;
 }
-PopupElementOpen.addEventListener('click', ItsPopupOpen); // обработчик по клику для открытытия поп-апа
-
-
+popupElementOpen.addEventListener('click', itsPopupOpen); // обработчик по клику для открытытия поп-апа
 //функция закрытия поп-апа
-const ItsPopupClose = function () {
-  PopupElement.classList.remove('popup_open');
+const itsPopupClose = function () {
+  popupElement.classList.remove('popup_open');
 }
-PopupElementClose.addEventListener('click', ItsPopupClose); //обработчик по клику для закрытия поп-апа
+popupElementClose.addEventListener('click', itsPopupClose); //обработчик по клику для закрытия поп-апа
